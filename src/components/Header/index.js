deded1c6ca3605a07fed10e2798d6a128c475268
moklick <info@moritzklack.com>
2018-08-31 17:39:52 +0200

    feat(app): add menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,36 @@
-import React, { PureComponent } from 'react';
-import { Box, Text } from 'rebass';
+import React, { PureComponent, Fragment } from 'react';
+import MenuIcon from 'react-feather/dist/icons/menu';
+import {
+  Box, Text, Flex, Drawer, Relative, Absolute, Close
+} from 'rebass';
+
+import Menu from '~/components/Menu';
 
 class Header extends PureComponent {
+  state = {
+    isMenuOpen: false
+  }
+
+  toggleMenu = () => {
+    this.setState(prevState => ({
+      isMenuOpen: !prevState.isMenuOpen
+    }));
+  }
+
   render() {
     return (
-      <Box p={3} bg="blue" color="white">
-        <Text>Moving Local</Text>
-      </Box>
+      <Fragment>
+        <Box p={3} bg="blue" color="white">
+          <Flex alignItems="center">
+            <MenuIcon onClick={this.toggleMenu} />
+            <Text ml={2}>Moving Local</Text>
+          </Flex>
+        </Box>
+        <Menu
+          open={this.state.isMenuOpen}
+          onClose={this.toggleMenu}
+        />
+      </Fragment>
     );
   }
 }
---
+++ src/components/Menu/index.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import {
+  Text, Drawer, Relative, Absolute, Close
+} from 'rebass';
+
+const StyledDrawer = styled(Drawer)`
+  box-shadow: 1px 0 6px 2px rgba(0, 0, 0, 0.1);
+`;
+
+class Menu extends PureComponent {
+  render() {
+    return (
+      <StyledDrawer
+        open={this.props.open}
+        side="left"
+        p={3}
+        bg="white"
+        color="black"
+      >
+        <Relative>
+          <Absolute right={5} top={0}>
+            <Close px={0} py={0} onClick={this.props.onClose} />
+          </Absolute>
+          <Text>Menu</Text>
+        </Relative>
+      </StyledDrawer>
+    );
+  }
+}
+
+export default Menu;
