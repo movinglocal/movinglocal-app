b251bddc88d0a888b9f68e86d2c28e796a4d06cd
g-div <g-div@anche.no>
2018-09-05 11:57:06 +0200

    feat(feed): add 'Load more' button for pagination

--- src/Store.js
+++ src/Store.js
@@ -3,21 +3,40 @@ import fetch from 'unfetch';
 
 export const Store = createStore({
   isLoading: true,
-  data: false
+  data: [],
+  page: { pageSize: 10, pageStart: 0 }
 });
 
 export const actions = () => ({
-  loadData: async () => {
-    let data = null;
+  loadData: async ({ page, data }) => {
+    let res = null;
+    const { pageSize, pageStart } = page;
     try {
-      const limit = 10;
-      data = await fetch(`https://movinglocal-api.herokuapp.com/article?_limit=${limit}`)
+      res = await fetch(`https://movinglocal-api.herokuapp.com/article?_limit=${pageSize}&_start=${pageStart}`)
         .then(r => r.json())
-        .then(r => r.filter(e => !e.ignored));
+        .then(r => r.filter(e => !e.ignored))
+        .then(r => data.concat(r));
     } catch (err) {
       console.log(err);
     }
-    return { data, isLoading: false };
+    return { data: res, isLoading: false };
+  },
+
+  incrementPage: ({ page }) => (
+    {
+      page: {
+        pageSize: page.pageSize,
+        pageStart: page.pageStart + page.pageSize
+      }
+    }
+  ),
+
+  loadNextPage: async ({ page, data }) => {
+    const { incrementPage, loadData } = actions();
+
+    const { page: p } = incrementPage({ page });
+    const { data: d } = await loadData({ page, data });
+    return { page: p, data: d };
   }
 });
 
--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,10 +1,16 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'unistore/react';
-import { Text } from 'rebass';
+import { Text, Button } from 'rebass';
 
 import { actions } from '~/Store';
 import FeedItem from '~/pages/Feed/components/FeedItem';
 import ScrollWrapper from '~/components/ScrollWrapper';
+import styled from 'styled-components';
+
+const FullWidthButton = styled(Button)`
+border-radius: 0;
+width: 100%;
+`;
 
 function renderItems(items) {
   return items.map((item, i) => <FeedItem {...item} key={i} />);
@@ -14,17 +20,26 @@ function renderLoader() {
   return <Text textAlign="center" pt={2}>Lade Daten ...</Text>;
 }
 
+function renderButton(action) {
+  return (
+    <FullWidthButton onClick={action}>
+      Load more...
+    </FullWidthButton>
+  );
+}
+
 class Feed extends PureComponent {
   componentDidMount() {
     this.props.loadData();
   }
 
   render() {
-    const { isLoading, data } = this.props;
+    const { isLoading, data, loadNextPage } = this.props;
 
     return (
       <ScrollWrapper bg="gray">
         {isLoading ? renderLoader() : renderItems(data)}
+        {!isLoading && renderButton(loadNextPage)}
       </ScrollWrapper>
     );
   }
