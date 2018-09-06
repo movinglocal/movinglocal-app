96ee02bfa21f2708f39d6ace083510e2f47a2540
g-div <g-div@anche.no>
2018-09-06 13:34:19 +0200

    feat(api): add source filters

--- src/Store.js
+++ src/Store.js
@@ -11,16 +11,21 @@ export const Store = createStore({
       { label: 'Datum', value: 'date:DESC' },
       { label: 'Quelle', value: 'source:ASC' }
     ]
-  }
+  },
+  sources: []
 });
 
 export const actions = () => ({
-  loadData: async ({ page, sortOptions }) => {
+  loadData: async ({ page, sortOptions, sources }) => {
     let data = null;
     const { pageSize, pageStart } = page;
     const { current } = sortOptions;
+    let url = `https://movinglocal-api.herokuapp.com/article?_limit=${pageSize}&_start=${pageStart}&_sort=${current}`;
+    sources.forEach((source) => {
+      if (!source.active) url = url.concat(`&source_ne=${source.id}`);
+    });
     try {
-      data = await fetch(`https://movinglocal-api.herokuapp.com/article?_limit=${pageSize}&_start=${pageStart}&_sort=${current}`)
+      data = await fetch(url)
         .then(r => r.json())
         .then(r => r.filter(e => !e.ignored))
     } catch (err) {
@@ -61,10 +66,45 @@ export const actions = () => ({
     sortOptions.current = event.target.value;
     const { data } = await loadData({ page, sortOptions });
     return { data, page, sortOptions };
+  },
+
+  search: async ({ page }, event) => {
+    const search = event.target.value;
+    console.log(search)
+    return { search };
+  }
+});
+
+export const settingsActions = () => ({
+  loadSources: async ({ sources }) => {
+    if (sources.length) return { sources };
+    try {
+      sources = await fetch('https://movinglocal-api.herokuapp.com/source')
+        .then(r => r.json())
+        .then(r => r.map((s) => {
+          s.active = true;
+          return s;
+        }));
+    } catch (err) {
+      console.log(err);
+    }
+    return { sources };
+  },
+
+  toggleSource: ({ sources }, id) => {
+    return {
+      sources: sources.map((s) => {
+        return {
+          ...s,
+          active: s.id === id ? !s.active : s.active
+        };
+      })
+    };
   }
 });
 
 export default {
   Store,
-  actions
+  actions,
+  settingsActions
 };
--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,15 +1,59 @@
 import React, { PureComponent } from 'react';
-import { Heading } from 'rebass';
+import {
+  Heading,
+  Subhead,
+  Text,
+  Switch,
+  Flex,
+  Box
+} from 'rebass';
 import ScrollWrapper from '~/components/ScrollWrapper';
 
+import { connect } from 'unistore/react';
+import { settingsActions } from '~/Store';
+
+function renderSource(source, toggleSource) {
+  const {
+    id, name, location, active
+  } = source;
+
+  return (
+    <Flex bg="white" p={2} m={2} key={id}>
+      <Box width={3 / 4} px={2}>
+        <Text fontSize={2}>
+          {name}
+        </Text>
+        <Text fontSize={1}>
+          {location.name}
+        </Text>
+      </Box>
+      <Box>
+        <Switch checked={active} onClick={() => toggleSource(id)} />
+      </Box>
+    </Flex>
+  );
+}
+
 class Settings extends PureComponent {
+  componentDidMount() {
+    this.props.loadSources();
+  }
+
   render() {
+    const { sources, toggleSource } = this.props;
     return (
       <ScrollWrapper p={3}>
         <Heading>Einstellungen</Heading>
+        <Subhead>Quellen:</Subhead>
+        {sources.map(s => renderSource(s, toggleSource))}
       </ScrollWrapper>
     );
   }
 }
 
-export default Settings;
+export default connect(
+  state => ({
+    sources: state.sources
+  }),
+  settingsActions
+)(Settings);
