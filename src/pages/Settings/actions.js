445941e4a174846f3578300be0701c1fcd6803eb
g-div <g-div@anche.no>
2018-09-07 16:56:55 +0200

    feat(app): save settings in IndexedDB

--- src/pages/Settings/actions.js
+++ src/pages/Settings/actions.js
@@ -1,14 +1,22 @@
 import { loadSources } from '~/services/api';
+import { add, get, remove } from '~/services/db';
+
+const COLLECTION = 'sources';
 
 export const settingsActions = () => ({
   loadSources: state => loadSources(state),
 
-  toggleSource: ({ sources }, id) => ({
-    sources: sources.map(s => ({
-      ...s,
-      active: s.id === id ? !s.active : s.active
-    }))
-  })
+  toggleSource: async ({ sources }, id) => {
+    const found = sources.find(s => s.id === id)
+    if (found.active) await add(COLLECTION, { id, active: false });
+    else await remove(COLLECTION, id);
+    return {
+      sources: sources.map(s => ({
+        ...s,
+        active: s.id === id ? !s.active : s.active
+      }))
+    };
+  }
 });
 
 export default {
