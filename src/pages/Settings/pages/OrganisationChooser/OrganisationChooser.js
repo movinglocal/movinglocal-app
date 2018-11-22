444047472b0b30d958bccea93bc137587fa08d2e
g-div <g-div@anche.no>
2018-11-22 16:09:10 +0100

    feat(settings): display collective and media separately

--- src/pages/Settings/pages/OrganisationChooser/OrganisationChooser.js
+++ src/pages/Settings/pages/OrganisationChooser/OrganisationChooser.js
@@ -55,12 +55,20 @@ class OrganisationChooser extends PureComponent {
   render() {
     const { isOnboarding, nextStep } = this.props;
     const { organisations } = this.state;
+    const media = organisations.filter(o => o.category === 'News');
+    const collective = organisations.filter(c => c.category === 'Kollektiv');
 
     return (
       <Box mb={this.props.isOnboarding ? 0 : 4}>
-        <Heading mb={3}>Organisationen auswählen:</Heading>
+        <Heading mb={3}>Welchen Medien willst du folgen?</Heading>
+        <Text my={3}>Wähle die Medien aus, deren Molos (Nachrichten und Informationen) dich interessieren.</Text>
         <Box mb={3}>
-          {organisations.map(d => this.renderOrganisation(d))}
+          {media.map(d => this.renderOrganisation(d))}
+        </Box>
+        <Heading mb={3}>Welchen Kollektiven willst du folgen?</Heading>
+        <Text my={3}>Wähle die Kollektive aus, deren Molos (Nachrichten und Informationen)dich interessieren.</Text>
+        <Box mb={3}>
+          {collective.map(d => this.renderOrganisation(d))}
         </Box>
         {isOnboarding && (
           <Flex>
